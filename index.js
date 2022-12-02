46dee69e4949194e6ed8e604be58ae9ad35f0301
JuanTrop <jortiza2@unicartagena.edu.co>
2022-12-02 18:12:06 -0500

    category toggle

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const menuProductList = document.getElementById('MenuProductList');
 const productCategories = document.getElementById('ProductCategories');
+const productsSection = document.querySelectorAll('.products__section');
 
 menuProductList.addEventListener('mouseover', () => {
     productCategories.classList.add('shown');
@@ -8,4 +9,11 @@ menuProductList.addEventListener('mouseover', () => {
 
 menuProductList.addEventListener('mouseleave', () =>{
     productCategories.classList.remove('shown');
+});
+
+//Click to pop the category sublist
+productsSection.forEach((section) =>{
+    section.children[0].addEventListener('click', (e) => {
+        section.children[1].classList.toggle('hideSubList');
+    });
 });
